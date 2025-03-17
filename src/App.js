f6bb036cb8e8fbc5a8a8e41d96022028418e33ac
mayv <hjwoo0812@naver.com>
2025-03-17 13:13:57 +0900

    side bar change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Play from "./pages/Play";
 import NavBar from "./components/NavBar";
+import "./App.css";
 
 function App() {
   return (
